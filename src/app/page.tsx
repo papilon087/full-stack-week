b01a10dcc230c3a5d8a1152d06e8d830cb302e11
papilon087 <ademirgus@gmail.com>
2023-07-08 15:00:41 -0300

    feat: add quick search components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import QuikiSearch from "./components/QuickSearch";
 import TripSearch from "./components/TripSearch";
 
 export default function Home() {
   return (
     <div className="">
       <TripSearch />
+      <QuikiSearch />
     </div>
   );
 }
