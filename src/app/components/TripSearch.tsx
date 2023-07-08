491895430e69f5a9c614ae8ddd8e29ac1e56fd1e
papilon087 <ademirgus@gmail.com>
2023-07-08 14:41:53 -0300

    feat: add initial search trip

---
+++ src/app/components/TripSearch.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React from "react";
+
+import Input from "@/components/Input";
+import DataPicker from "@/components/DataPicker";
+import CurrencyInput from "@/components/CurrencyInput";
+import Button from "@/components/Button";
+
+const TripSearch = () => {
+  return (
+    <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat">
+      <h1 className="font-semibold text-2xl text-primaryDarker text-center">
+        Encontre sua próxima <span className="text-primary">viagem!</span>
+      </h1>
+
+      <div className="flex flex-col gap-4 mt-5">
+        <Input placeholder="Onde você quer ir?" />
+
+        <div className="flex gap-4">
+          <DataPicker
+            placeholderText="Data de Ida"
+            onChange={() => {}}
+            className="w-full"
+          />
+          <CurrencyInput placeholder="Orçamento" />
+        </div>
+
+        <Button>Buscar</Button>
+      </div>
+    </div>
+  );
+};
+
+export default TripSearch;
