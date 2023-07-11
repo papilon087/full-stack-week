6156e3c13f3123c55f826475b060796abc5f41e5
papilon087 <ademirgus@gmail.com>
2023-07-11 16:26:33 -0300

    chore: recreating the db:seed trips

--- src/app/components/QuickSearch.tsx
+++ src/app/components/QuickSearch.tsx
@@ -5,11 +5,11 @@ const QuikiSearch = () => {
   return (
     <div className="container mx-auto p-5">
       <div className="flex items-center">
-        <div className="w-full h-[2px] bg-grayLighter"></div>
+        <div className="w-full h-[1px] bg-grayLighter"></div>
         <h2 className="px-5 font-medium text-grayPrimary whitespace-nowrap">
           Tente pesquisar por
         </h2>
-        <div className="w-full h-[2px] bg-grayLighter"></div>
+        <div className="w-full h-[1px] bg-grayLighter"></div>
       </div>
 
       <div className="flex w-full justify-between mt-5">
@@ -18,17 +18,17 @@ const QuikiSearch = () => {
           <p className="text-sm text-grayPrimary">Hotel</p>
         </div>
 
-        <div className="fle flex-col items-center gap-1">
+        <div className="flex flex-col items-center gap-1">
           <Image width={35} height={35} src="/farm-icon.png" alt="Hotel" />
           <p className="text-sm text-grayPrimary">Fazenda</p>
         </div>
 
-        <div className="fle flex-col items-center gap-1">
+        <div className="flex flex-col items-center gap-1">
           <Image width={35} height={35} src="/cottage-icon.png" alt="Hotel" />
           <p className="text-sm text-grayPrimary">Chále</p>
         </div>
 
-        <div className="fle flex-col items-center gap-1">
+        <div className="flex flex-col items-center gap-1">
           <Image width={35} height={35} src="/inn-icon.png" alt="Hotel" />
           <p className="text-sm text-grayPrimary">Pousada</p>
         </div>
--- src/app/components/RecommendeTrips.tsx
+++ src/app/components/RecommendeTrips.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { Trip } from "@prisma/client";
-import { prisma } from "@/lib/prisma";
-
 import TripItem from "@/components/TripItem";
+import { prisma } from "@/lib/prisma";
+import { Trip } from "@prisma/client";
+import React from "react";
 
 async function getTrips() {
   const trips = await prisma.trip.findMany({});
+
   return trips;
 }
 
@@ -15,14 +15,14 @@ const RecommendedTrips = async () => {
   return (
     <div className="container mx-auto p-5">
       <div className="flex items-center">
-        <div className="w-full h-[2px] bg-grayLighter"></div>
+        <div className="w-full h-[1px] bg-grayLighter"></div>
         <h2 className="px-5 font-medium text-grayPrimary whitespace-nowrap">
           Destinos Recomendados
         </h2>
-        <div className="w-full h-[2px] bg-grayLighter"></div>
+        <div className="w-full h-[1px] bg-grayLighter"></div>
       </div>
 
-      <div className="flex flex-col items-center mt-5 gap-5">
+      <div className="flex flex-col items-center mt-5 lg:mt-12 gap-5 lg:flex-row gap lg:flex-wrap lg:justify-center lg:gap-10">
         {data.map((trip: Trip) => (
           <TripItem key={trip.id} trip={trip} />
         ))}
@@ -30,5 +30,4 @@ const RecommendedTrips = async () => {
     </div>
   );
 };
-
 export default RecommendedTrips;
